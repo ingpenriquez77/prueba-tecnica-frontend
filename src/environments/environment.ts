5f75d720aebeb4a6149a7e938222050b64a2d1c1
Pedro Rafael Enriquez Nevarez <207899489+pedro-enriquez_coppel@users.noreply.github.com>
2026-07-13 12:31:15 -0700

    Update environment.ts
    Fix: actualizar apiUrl local por el servidor real de Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // 🚀 URL base apuntando al servidor de desarrollo de tu API en Laravel 11
-  apiUrl: 'http://127.0.0.1:8000/api'
+  //  URL base apuntando al servidor de desarrollo de tu API en Laravel 11
+  apiUrl: 'https://api-backend-dez2.onrender.com/api'
 };
